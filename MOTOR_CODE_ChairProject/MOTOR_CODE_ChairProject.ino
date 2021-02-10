8528687a7be2c7d1bc1381c94ec01c68f724082e
CML lab <cognitivemotorlab@gmail.com>
2021-02-10 11:52:50 -0500

    Working version of chair controller and rotary encoder

---
+++ MOTOR_CODE_ChairProject/MOTOR_CODE_ChairProject.ino
@@ -0,0 +1,266 @@
+/* MOTOR_CODE_ChairProject
+   -*- mode: C++ -*-
+
+  ***PURPOSE***
+    This program is intended to control the vestibular rotating chair. 
+    Prompt use for input, rotate to desired endpoint, display relevant information.
+    
+    2021 - Evan C. Snow (For use in Erica Barhost-Cates PhD. and Aaron Wong PhD. study on Localization)
+           with additional mods by Aaron L. Wong
+
+  ***SCHEMATIC***
+    ~Arduino~
+    DIGITAL pin 6: PWM (+) to microstepper
+    DIGITAL pin 7: DIR (+) to microstepper
+    GND: PUL (-), DIR(-)
+
+    ~Microstepper~        _
+    Port 1: DIR (-)     |
+    Port 2: DIR (+)     | Logic Pins to Arduino
+    Port 3: PUL (-)     |
+    Port 4: PUL (+)    _|
+    Port 5: ENA (-)     | // Not used //
+    Port 6: ENA (+)    _| ~Can be used for Enabling/Disabling microstepper from Arduino HIGH/LOW DIGITAL pin output~
+                _
+    Switch 1: ON     |
+    Switch 2: ON     | Current Tweaking Switches: 4.28A (REF), 5.14A (PEAK)
+    Switch 3: OFF   _|
+    Switch 4: ON    _| Full vs Half Current Switch: FULL
+    Switch 5: ON     |
+    Switch 6: ON     | Microstepping Set Switches: 6400 steps/rev
+    Switch 7: OFF    |
+    Switch 8: ON    _|
+                  _
+    Port 7: A (+)      |
+    Port 8: A (-)      | Output to NEMA 34 stepper motor
+    Port 9: B (+)      |
+    Port 10: B (-)    _|
+    Port 11: V (+)     | Thick Black Wire [+] HIGH VOLTAGE (~36VDC)     --DO NOT TOUCH THESE WIRES IT WILL HURT--
+    Port 12: V (-)    _| Thick White Wire [-] HIGH VOLTAGE (~36V)
+
+    ~Power Supply~             _
+    Port 1: Lead (Black)        |
+    Port 2: Neutral (White)     | Power cord to power strip to wall. [120VAC]
+    Port 3: Ground (Green)______|
+    Port 4:                |
+    Port 5: V [-]          |
+    Port 6: //Not used     | HIGH VOLTAGE [36VDC] LOOP TO MICROSTEPPER
+    Port 7: V [+]          |
+    Port 8: //Not used     |
+    Port 9: //Not used    _|
+
+  ***INSTRUCTIONS***
+
+    -CONNECT "Motor Arduino" (Arduino Uno COM4) and open Tools > Serial Monitor. Once clicking this the Arduino board will
+    reboot (This can be a soft reset i.e. close Serial Monitor and open Serial Monitor to effectively RESET).
+
+    -Follow instructions on Serial Monitor. There is a field on top to send Serial data to the Arduino board. Click SEND
+    button once done typing in desired input command.
+
+    -When finished, click anywhere in the Serial Monitor output and ctr + A then ctr + C to copy all outputs.
+
+    -Paste in a .txt file or whatever type of file you want the data in
+
+    -BOOM, you did it!
+
+
+  ***NOTES***
+    To add AccelStepper library, Go to Tools > Manage Libraries. Search for AccelStepper and install the latest version.
+
+    To communicate with the secondary Arduino (which is reading the rotary encoder), connect the two arduinos via the
+    GND pin and one of the digital pins to act as an enable line/trigger signal. Here we use pin 12 as defined by triggerPinRE.
+
+*/
+
+#include <AccelStepper.h>
+//#include <avr/wdt.h> //for library containing watchdog
+
+// Initialize the library with number of interface pins
+AccelStepper stepper(1, 6, 7); // 1 is mode, 6 is PWM (+) pin and 7 is dir (+) connection, (-) counterparts to GND
+
+float movementSteps = 0;
+bool inputReceived = 0;
+float inputMove = 0;
+float oneRevSteps = 51200;
+int trial = 1;
+const int triggerPinRE = 12;
+int triggerPinREState = LOW;
+
+#define BAUD_RATE 115200
+//38400;
+
+void setup()
+{
+    Serial.begin(BAUD_RATE);             //Initialize serial communication between Arduino UNO and USB port on Windows computer
+
+    pinMode(triggerPinRE, OUTPUT);    //Initialize digital trigger pin to talk between microcontrollers
+
+    // Stepper Parameters
+    stepper.setMaxSpeed(8000);
+    stepper.setAcceleration(1000);
+    //oneRevSteps = 6400;             //One full rotation is 6400 steps
+
+    InitializeProgram();
+}
+
+
+//recall that we can also use a void serialEvent() {} loop to capture serial data as well
+void loop()
+{
+    Serial.print("\n");
+    Serial.print("<-*-*- TRIAL "); Serial.print(trial); Serial.println(" -*-*->");
+    delay(1000);
+    inputReceived = false;  // set at zero for every iteration to pop back into the while loop
+    inputMove = 0;      // set at zero to begin every iteration... also helps to make sure there are no accidental movements
+
+    //Get input for movement
+    GetInput();
+
+    //Move to end position
+    MoveChair();
+
+    //Record patient output
+    RecordOutput();
+
+    trial++;    // increase trial iteration
+    delay(1000);
+}
+
+//-------FUNCTIONS-------\\
+
+void InitializeProgram() {
+    //Give time for power supply to reach stable voltage
+    Serial.println(""); //clear Serial buffer
+    Serial.print("Initializing");
+    delay(1000); Serial.print("."); delay(1000); Serial.print("."); delay(1000); Serial.println(".");
+
+    // set the LED with the ledState of the variable:
+    triggerPinREState = LOW;        //initialize the trigger signal LOW from the motor digital pin to the rotary encoder digital pin using digitalRead(Pin, State [HIGH, LOW])
+    digitalWrite(triggerPinRE, triggerPinREState);  //Make sure digital pin 12 initializes to LOW
+
+
+    //Give information about readiness of program and feedback to the user.
+    Serial.print("The current location is: "); DegreeCalculate(); Serial.println(" degrees.");
+    Serial.println("Enter anything in the serial monitor and click SEND to begin the main program.");
+    while (Serial.available() == 0) {}
+    Serial.println("Begin testing protocol.");
+    delay(500);
+}
+
+void GetInput() {
+    while (inputReceived == false) {
+
+        //There's a bug EXCLUSIVE to the first iteration that skips the next serial call, so this keeps unexpected stuff from happeing on first iteration
+        if (trial == 1) {
+            while (Serial.available() == 0) {} //hold at this point in the code until user clicks SEND on Serial Monitor
+            inputMove = Serial.parseInt();    //store input
+        }
+        Serial.println("How far would you like to move RELATIVE to the current position? [CCW (+) CW(-)]");
+        Serial.println("(Please input a non-zero value from -360 to 360 degrees and click SEND.)");  //prompt user for input
+        while (Serial.available() == 0) {} //hold at this point in the code until user clicks SEND on Serial Monitor
+        inputMove = Serial.parseInt();    //store input
+
+        if (((inputMove >= -360) && (inputMove <= 360)) && (inputMove != 0)) {
+            //Serial.print("Moving "); Serial.print(inputMove);
+            //Serial.print(" degrees. Starting Position: ");
+            //DegreeCalculate(); Serial.print(" degrees. Ending Position: ");
+            //DegreeCalculate2(); Serial.println(" degrees.");
+
+            delay(1000);
+            Serial.println("--- BEGINNING MOVEMENT ---");
+
+            // set the LED with the ledState of the variable:
+            triggerPinREState = HIGH;        //trigger a signal from the motor digital pin to the rotary encoder digital pin using digitalRead(Pin, State [HIGH, LOW])
+            digitalWrite(triggerPinRE, triggerPinREState);  //Set digital pin 12 to HIGH
+            inputReceived = true;
+        }
+        else {
+            Serial.println("You did not enter a non-zero value between -360 and 360 degrees.");
+            delay(1000);
+        }
+    }
+}
+
+void MoveChair() {
+
+
+    //Serial.print("oneRevSteps: "); Serial.println(oneRevSteps);
+
+    movementSteps = (float)(oneRevSteps * (inputMove / 360));
+    Serial.print("inputMove: "); Serial.print(inputMove); Serial.println(" degrees");
+    Serial.print("movementSteps: "); Serial.print(movementSteps); Serial.println(" steps");
+    Serial.print("StartingPosition: "); DegreeCalculate(); Serial.println(" degrees");
+    Serial.print("EndingPosition: "); DegreeCalculate2(); Serial.println(" degrees");
+    movementSteps += stepper.currentPosition();
+    //Serial.print("movementSteps: "); Serial.println(movementSteps);
+
+    stepper.moveTo(movementSteps);
+    while (stepper.distanceToGo() != 0) { //wait until movement is finished... making sure there is not skip
+        stepper.run();
+    }
+    if (stepper.distanceToGo() == 0) {
+        Serial.println("--- FINISHED MOVEMENT ---");
+        triggerPinREState = LOW;
+        digitalWrite(triggerPinRE, triggerPinREState);
+        Serial.print("The current location is: "); DegreeCalculate(); Serial.println(" degrees.");
+    }
+}
+
+void RecordOutput() {
+    Serial.println("What was the Patient response?");
+    while (Serial.available() == 0) {} //hold at this point in the code until user clicks SEND on Serial Monitor
+    int patientResponse = Serial.parseInt();    //store patient response integer
+    Serial.print("patientResponse:  "); Serial.print(patientResponse); Serial.println(" degrees.");
+}
+
+void DegreeCalculate() {
+    float degreesAt = 0;
+    degreesAt = (float)stepper.currentPosition() / oneRevSteps * 360;
+
+    if (degreesAt >= 0) {
+        int n = floor(degreesAt / 360);
+        degreesAt = degreesAt - 360 * n;
+        Serial.print(degreesAt);
+    }
+    else if (degreesAt < 0) {
+        int n = floor(-degreesAt / 360);
+        degreesAt = (360 + degreesAt + (360 * n));
+        Serial.print(degreesAt);
+    }
+    return;
+}
+
+void DegreeCalculate2() {
+    float degreesAt2 = 0;
+    degreesAt2 = (float)stepper.currentPosition() / oneRevSteps * 360;
+    degreesAt2 += inputMove;
+    if (degreesAt2 >= 0) {
+        int n = floor(degreesAt2 / 360);
+        degreesAt2 = degreesAt2 - 360 * n;
+        Serial.print(degreesAt2);
+    }
+    else if (degreesAt2 < 0) {
+        int n = floor(-degreesAt2 / 360);
+        degreesAt2 = (360 + degreesAt2 + (360 * n));
+        Serial.print(degreesAt2);
+    }
+    return;
+}
+
+/*  POSSIBLE SETUP IF WANT TO SAVE VARIABLE... I JUST SET UP A GLOBAL VARIABLE TO UPDATE (plenty of resources left)
+
+    int degreeCalculate(){
+    float degreesAt = 0;
+    degreesAt = (float)stepper.currentPosition()/1600*180;
+    if (degreesAt >= 0){
+    int n = floor(degreesAt/360);
+    degreesAt = degreesAt-360*n;
+    return degreesAt;
+    }
+    else if (degreesAt < 0){
+    int n = floor(-degreesAt/360);
+    degreesAt = (360+degreesAt+(360*n));
+    return degreesAt;
+    }
+    }
+*/
---
+++ rotary_encoder_reader/rotary_encoder_reader.ino
@@ -0,0 +1,252 @@
+/*
+    Read optical incremental rotary encoder output.
+    This code was modified from https://gist.github.com/kinverarity1/8ca395b85bb63e1e7471d7db0284c30b
+
+    Serial output columns:
+
+    1. Absolute encoder position (pulses, integer) from startup
+    2. Change in encoder position from trial start (pulses, integer)
+    3. Encoder position at trial start (degrees, float)
+    4. Change in motor shaft position (degrees, float)
+    5. Time since trial start in (microseconds, integer)
+    6. Motor Shaft Speed (degrees per second, float)
+
+    This code tracks absolute position of the chair in pulses from the time of startup, and converts that to
+    relative position and velocity in degrees based on the current position and time at the start of each trial.
+
+    volatile directive is used for variables that are modified in the
+    interrupt service routines.
+
+*/
+
+// Pin definitions.
+// - enc_a is ENC Signal A line (Arduino digital pin 2)
+// - enc_b is ENC Signal B line (Arduino digital pin 3)
+#define ENC_A  2
+#define ENC_B  3
+#define triggerPinRE 12  //trigger/enable line from the motor driver Arduino
+// #define trialcountpin 13 //this pin will change state every X trials to help us keep count
+
+// Main loop refresh period.
+#define REFRESH_MS  50
+
+// Main serial data connection to computer.
+#define BAUD_RATE   115200
+
+// Encoder signal line states
+volatile boolean state_a = 0;
+volatile boolean state_b = 0;
+
+// Encoder position
+volatile int enc_pos = 0;
+int enc_pos_prev = 0;
+int enc_pos_change = 0;
+int enc_pos_trialstart = 0;
+float abs_pos_degrees = 0;
+float rel_pos_degrees = 0;
+float rel_pos_prev = 0;
+float rel_pos_change = 0;
+float abs_pos_trialstart = 0;
+
+
+float REratio = (200.0 / 268.7);  //ratio of circumference of rotary encoder wheel to motor shaft
+//the rotary encoder has a 200 mm circumference
+float REppr = 1000.0; //the rotary encoder has 1000 pulses/revolution
+
+// Timing
+unsigned long micros_current = 0;
+unsigned long micros_prev = 0;
+unsigned long micros_trialstart = 0;
+long micros_change = 0;
+
+// keep track of enable signal
+int readEnable = LOW;
+int readPriorState = LOW;
+int trial = 0;  // note that rather than send the trial number over (which will take ~8 pins), we will just count enables
+
+//keep track of whether we are actively printing the encoder output or not
+bool REenabled;
+bool REhold;
+unsigned long REholdstime;
+unsigned long REholdctime;
+long REholdetime;
+
+
+void setup()
+{
+    pinMode(ENC_A, INPUT);
+    pinMode(ENC_B, INPUT);
+
+    pinMode(triggerPinRE, INPUT);
+
+    state_a = (boolean) digitalRead(ENC_A);
+    state_b = (boolean) digitalRead(ENC_B);
+
+    readEnable = digitalRead(triggerPinRE);
+
+    attachInterrupt(0, interrupt_enc_a, CHANGE);
+    attachInterrupt(1, interrupt_enc_b, CHANGE);
+
+    micros_prev = micros();
+    micros_trialstart = micros();
+
+    Serial.begin(BAUD_RATE);
+
+    Serial.println("Ready to begin recording.");
+
+    REenabled = 0;
+}
+
+
+void loop()
+{
+
+    int n;
+    float bounded_trialstart;
+    unsigned long etime;
+
+    readEnable = digitalRead(triggerPinRE);
+    if (readPriorState == LOW && readEnable == HIGH) // if the enable signal just went high (START RECORDING)
+    {
+
+        //reset trial-based tracking variables back to 0
+        rel_pos_degrees = 0;
+        abs_pos_trialstart = abs_pos_degrees;
+        enc_pos_trialstart = enc_pos;
+        rel_pos_prev = 0;
+        rel_pos_change = 0;
+
+        // Timing
+        micros_trialstart = micros();
+        etime = 0;
+
+        //enable updates
+        REenabled = true;
+        REhold = false;
+
+        trial = trial + 1;
+        Serial.print("\n");
+        Serial.print("<-*-*- TRIAL "); Serial.print(trial); Serial.println(" -*-*->");
+
+        readPriorState = readEnable;
+    }
+    else if (readPriorState == HIGH && readEnable == LOW) // if the enable signal just went low (STOP RECORDING)
+    {
+        //REenabled = false; // stop updating the position information
+        REhold = true;
+        REholdstime = micros(); //start a timer to delay when recordin shuts off
+        readPriorState = readEnable; // stop
+    }
+    else if (REhold)
+    {
+        REholdctime = micros();
+        if (REholdctime < REholdstime) {
+            REholdetime = REholdctime + (4294967295 - REholdstime);
+        } else {
+            REholdetime = REholdctime - REholdstime;
+        }
+
+        if (REholdetime > 1000000)  //the timer has expired, stop recording
+        {
+            REhold = false;
+            REenabled = false; // stop updating the position information
+        }
+      
+    }
+    
+
+    //REenabled = 1;
+
+
+    // Calculate change in encoder position.
+    //enc_pos_change = enc_pos - enc_pos_prev;
+    //enc_pos_change = abs(enc_pos_change);
+    //abs_pos_degrees = enc_pos * .00074074 * 360; //(1/1350)
+    abs_pos_degrees = enc_pos * (360.0 / REppr) * REratio; // number of pulses * (deg/pulse) * (motor rotation / encoder rotation)
+
+
+    if (REenabled)
+    {
+
+        // Calculate elapsed time
+        micros_current = micros();
+        if (micros_current < micros_prev) {
+            micros_change = micros_current + (4294967295 - micros_prev);
+        } else {
+            micros_change = micros_current - micros_prev;
+        }
+        //calculate elapsed time since trial start
+        if (micros_current < micros_trialstart) {
+            etime = micros_current + (4294967295 - micros_trialstart);
+        } else {
+            etime = micros_current - micros_trialstart;
+        }
+        
+        //calculate the relative position change (pulses) since trial start
+        enc_pos_change = enc_pos - enc_pos_trialstart;
+
+        // Calculate relative position change (degrees) since trial start
+        rel_pos_degrees = abs_pos_degrees - abs_pos_trialstart; // this is bounded to [-360 360] by the motor_driver code
+        rel_pos_change = abs(rel_pos_degrees - rel_pos_prev);
+
+        //compute bounded absolute position at trial start
+        if (abs_pos_trialstart >= 0)
+        {
+            n = floor(abs_pos_trialstart / 360);
+            bounded_trialstart = abs_pos_trialstart - 360 * n;
+        }
+        else if (abs_pos_trialstart < 0)
+        {
+            n = floor(-abs_pos_trialstart / 360);
+            bounded_trialstart = (360 + abs_pos_trialstart + (360 * n));
+        }
+
+        // Emit data
+        Serial.print(enc_pos);
+        Serial.print("\t");
+        Serial.print(enc_pos_change);
+        Serial.print("\t");
+        Serial.print(bounded_trialstart, 3);
+        Serial.print("\t");
+        Serial.print(abs_pos_degrees, 3);
+        Serial.print("\t");
+        Serial.print(etime);
+        Serial.print("\t");
+        Serial.print(rel_pos_change / (micros_change / 1e6), 3);
+        //Serial.print(enc_pos);
+        //Serial.print("\t");
+        //Serial.print(enc_pos_change);
+        //Serial.print("\t");
+        //Serial.print(micros_current);
+        //Serial.print("\t");
+        //Serial.print(micros_change);
+        //Serial.print("\t");
+        //Serial.print(enc_pos_change / (micros_change / 1e6));
+        Serial.print("\n");
+
+        micros_prev = micros_current;
+        rel_pos_prev = rel_pos_degrees;
+
+    }
+
+    //enc_pos_prev = enc_pos;
+
+
+    delay(REFRESH_MS);
+}
+
+
+// Detect pulses from depth encoder.
+
+void interrupt_enc_a()
+{
+    if (!state_a) {
+        state_b ? enc_pos++ : enc_pos--;
+    }
+    state_a = !state_a;
+}
+
+void interrupt_enc_b()
+{
+    state_b = !state_b;
+}
